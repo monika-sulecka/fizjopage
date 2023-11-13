ac35909ab98a153525dc7b5938a7f7e2ea6cb32d
monika-sulecka <120500604+monika-sulecka@users.noreply.github.com>
2023-11-13 11:04:59 +0100

    change in paragraf on dashboard

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -8,9 +8,9 @@ export default class extends AbstractView {
 
 	async getHtml() {
 		return `
-            <h1>Welcome back, Dom</h1>
+            <h1>Miło Cię widzieć</h1>
             <p>
-                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
+                Zapraszamy serdecznie na nasze spersonalizowane terapie, gdzie każdy znajdzie coś dla siebie. 
             </p>
             <p>
                 <a href="/posts" data-link>View recent posts</a>.
